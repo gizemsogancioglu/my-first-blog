f34edd77307954fcec7d0c763b26db75b27bbdac
gizemsogancioglu <gizemsogancioglu@gmail.com>
2016-03-13 02:49:46 +0200

    html changed

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-def post_list(request):     
-    return render(request, 'blog/post_list.html', {})
+from django.utils import timezone
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
